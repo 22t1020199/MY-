e8d25e8642f7d9ab08a5a83471ef9858b06e043d
Truong Duc Mai Linh <22t1020199@husc.edu.vn>
2026-04-17 08:35:21 +0700

    upp

--- config.py
+++
@@ -1,3 +0,0 @@
-# CẢNH BÁO: ĐÂY LÀ SAI LẦM BẢO MẬT
-DATABASE_PASSWORD = "Admin_Password_123!"
-API_KEY = "ghp_NgauNhien1234567890"
---
+++ login_service.py
@@ -0,0 +1,7 @@
+# File: login_service.py
+def connect_gmail():
+    email = "nhanvien1@gmail.com"
+    password = "hbiphubai123!"  
+    print(f"Đang kết nối tới {email}...")
+
+connect_gmail()
